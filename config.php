d77dda49e06666b51430b20b22f455acd5dfe08c
Welton Cardoso <weltonvcardoso@gmail.com>
2020-01-31 09:19:51 -0300

    Update config.php
    Ajustes config

--- config.php
+++ config.php
@@ -13,14 +13,14 @@ if(ENVIRONMENT == 'development') {
 } else {
 	$config['dbname'] = 'secatreambient02';
 	$config['host'] = 'mysql.secatreambiental.com.br';
-	$config['dbuser'] = 'secatrea02_add1';
-	$config['dbpass'] = 'eltim325';
+	$config['dbuser'] = '';
+	$config['dbpass'] = '';
 }
 //colocar email do vendedor ao final
 $config['pagseguro_seller'] = "emyr.mendes@gmail.com";
 //colocar appid e key do mp https://applications.mercadopago.com.br
-$config['mp_appid'] = '7162423875927553';
-$config['mp_key'] = 'Fs9SGZ5FTst9H1lSrsViPV9exls9Xinq';
+$config['mp_appid'] = '';
+$config['mp_key'] = '';
 
 \PagSeguro\Library::initialize();
 \PagSeguro\Library::cmsVersion()->setName("SecatreAmbiental")->setRelease("2.0.0");
@@ -31,9 +31,9 @@ $config['mp_key'] = 'Fs9SGZ5FTst9H1lSrsViPV9exls9Xinq';
 //urlpra pegar token sandbox.pagseguro.uol.com.br 
 //perfis de integracao/vendedor
 //emyr.mendes@gmail.com
-//9A36A46ADE534696BF20671C8AD22C82
-//\PagSeguro\Configuration\Configure::setAccountCredentials('emyr.mendes@gmail.com','9A36A46ADE534696BF20671C8AD22C82');
-\PagSeguro\Configuration\Configure::setAccountCredentials('emyr.mendes@gmail.com','880614A9CFCC4DDB9CFD21FE98022ACF');
+//
+//\PagSeguro\Configuration\Configure::setAccountCredentials('emyr.mendes@gmail.com','');
+\PagSeguro\Configuration\Configure::setAccountCredentials('emyr.mendes@gmail.com','');
 \PagSeguro\Configuration\Configure::setCharset('UTF-8');
 \PagSeguro\Configuration\Configure::setLog(true, 'pagseguro.log');
 ?>
